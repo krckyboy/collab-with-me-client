1dbae700711518f4dd26b61c2b0286a3dda8cece
krckyboy <dusan.todorovic.2016@gmail.com>
2021-06-15 15:18:53 +0200

    Included vue.config.js to import _vars.scss on every component.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,9 @@
 <template>
   <header class="header">
-    <router-link class="header__section header__section--left" to="/">
+    <router-link class="section section--left" to="/">
       <img src="@/assets/logo.svg" alt="logo" class="logo">
     </router-link>
-    <div class="header__section header__section--right">
+    <div class="section section--right">
       <router-link class="login" to="/login">Login</router-link>
       <router-link class="register" to="/register">Register</router-link>
     </div>
@@ -16,22 +16,32 @@ export default {
 };
 </script>
 
-<style lang="scss">
-.header {
+<style lang="scss" scoped>
+header {
   padding: 8px 24px;
 
-  &, &__section--right {
+  &, .section--right {
     display: flex;
     justify-content: space-between;
     align-items: center;
   }
 
-  &__logo {
+  .logo {
     width: 48px;
     height: 48px;
+  }
 
-    img {
-      width: 100%;
+  .section--right {
+    a {
+      font-weight: bold;
+    }
+
+    .register {
+      border-radius: 12px;
+      border: 3px solid $secondary-color;
+      color: $secondary-color;
+      padding: 12px;
+      margin-left: 24px;
     }
   }
 }
--- src/components/IllustrationSection.vue
+++ src/components/IllustrationSection.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="ill">
+  <section>
     <img :src="require('@/assets/' + imageName + '')"
          alt="title">
     <h1>{{ title }}</h1>
@@ -20,28 +20,29 @@ export default {
 };
 </script>
 
-<style lang="scss">
-.ill {
+<style lang="scss" scoped>
+section {
   &:first-of-type {
     margin-top: 48px;
   }
 
   margin-top: 148px;
-
-  h1 {
-    font-size: 36px;
-    font-weight: bold;
-    margin-top: 40px;
-  }
-
-  p {
-    font-size: 14px;
-    line-height: 24px;
-    margin-top: 24px;
-  }
-
-  .button {
-    margin-top: 32px;
-  }
 }
+
+h1 {
+  font-size: 36px;
+  font-weight: bold;
+  margin-top: 40px;
+}
+
+p {
+  font-size: 14px;
+  line-height: 24px;
+  margin-top: 24px;
+}
+
+.button {
+  margin-top: 32px;
+}
+
 </style>
